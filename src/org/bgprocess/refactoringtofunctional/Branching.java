1aefc0bb19fcf086c5b908a53571b3709e5e04c1
Andrew Parker <zaphod@Heart-of-Gold.home>
2011-11-06 16:09:55 +0000

    Added branching outline

---
+++ src/org/bgprocess/refactoringtofunctional/Branching.java
@@ -0,0 +1,24 @@
+package org.bgprocess.refactoringtofunctional;
+
+import org.junit.Test;
+
+public class Branching {
+    /**
+     * Branch: Data -> Function
+     */
+    
+    @Test public void
+    dispatch_from_data() {
+        
+    }
+    
+    @Test public void
+    dispatch_from_predicates() {
+        
+    }
+    
+    @Test public void
+    setup_in_one_place_and_inject_decision() {
+        
+    }
+}
